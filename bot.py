86522ef0a6f2a13c9daaee8d9c74ffc0f6021302
Andtheking <andreamcastronovo06@gmail.com>
2022-03-19 19:21:09 +0100

    Mi so rotto il cazzo di mettere commenti

--- bot.py
+++ bot.py
@@ -39,8 +39,7 @@ def main():
 
      dp.add_error_handler(error)
 
-     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-     updater.bot.setWebhook('https://stormy-bayou-12485.herokuapp.com/' + TOKEN)
+     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN, webhook_url="https://stormy-bayou-12485.herokuapp.com/" + TOKEN)
      updater.idle()
      
     
