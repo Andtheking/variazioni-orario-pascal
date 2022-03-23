88cd4d5d939e10c5f7eba13c678770f0d58e2a78
Andrea Maria <90281339+Andtheking@users.noreply.github.com>
2022-03-23 06:45:44 +0100

    Commit

--- bot.py
+++ bot.py
@@ -171,6 +171,8 @@ def main():
     dp.add_error_handler(error) # In caso di errore:
     
 
+    mandaMessaggio()
+
     #schedule.every().day.at("00:05").do(GetUrl)
 
     schedule.every().monday.at("06:30").do(mandaMessaggio)
