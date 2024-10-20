8cf091fdf83b853b96187b4e4c690e4bfd171698
Andrea Maria <andreamcastronovo06@gmail.com>
2024-10-20 20:58:06 +0200

    bruh

--- jobs/initialize.py
+++ jobs/initialize.py
@@ -1,9 +1,5 @@
 from telegram.ext import ContextTypes
 from utils.log import log
-from utils.regex_image import get_regex_image
-from requirements import config
-
-
 
 async def initialize(context: ContextTypes.DEFAULT_TYPE):
     log("Bot online at: https://t.me/" + context.bot.username)
